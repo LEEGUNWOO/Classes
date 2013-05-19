1c72c397c318eac191e381270eb5cabd80fdeac1
LEEGUNWOO <lgw4928@gmail.com>
2013-05-19 20:54:11 +0900

    ssss
    ssss

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -63,7 +63,7 @@ bool HelloWorld::init()
         // 2. Add a label shows "Hello World".
 
         // Create a label and initialize with string "Hello World".
-        CCLabelTTF* pLabel = CCLabelTTF::create("Hello jjj", "Arial", 24);
+        CCLabelTTF* pLabel = CCLabelTTF::create("Hello help", "Arial", 24);
         CC_BREAK_IF(! pLabel);
 
         // Get window size and place the label upper. 
